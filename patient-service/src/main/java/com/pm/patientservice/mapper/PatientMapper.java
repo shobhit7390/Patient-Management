742137677c0404731c3f3f7f6b9b11468d1eb3c0
shobhit6402 <vms6402@gmail.com>
2025-06-15 22:01:01 +0530

    Added repo and service layer

---
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -0,0 +1,17 @@
+package com.pm.patientservice.mapper;
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.model.Patient;
+
+public class PatientMapper {
+
+    public static PatientResponseDTO toDTO(Patient patient){
+        PatientResponseDTO patientDTO = new PatientResponseDTO();
+        patientDTO.setId(patient.getId().toString());
+        patientDTO.setEmail(patient.getEmail());
+        patientDTO.setName(patient.getName());
+        patientDTO.setAddress(patient.getAddress());
+        patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
+        return patientDTO;
+    }
+}
